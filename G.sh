193d60e2c4b099426c3802922921872394cf2143
cds-amal <cds.sudama@gmail.com>
2022-04-16 18:59:05 -0400

    more dirty

--- G.sh
+++ G.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+
+# quick and dirty
+
 trap ctrl_c INT
 
 outfile=$1
 
 function ctrl_c () {
   printf "\n"
+  cp $$.tmp xyz
   grep "^..._" $$.tmp | sort -r | uniq -c > $outfile
   grep "^..._" $$.tmp | sort -r | uniq -c | awk '{ sum += $1 } END { print sum " total calls"}' >> $outfile
   rm $$.tmp
@@ -14,5 +18,6 @@ function ctrl_c () {
 }
 
 ganache \
+  -v \
   -m "window twenty cat alone physical foam smoke annual collect pluck educate course" \
   -p 8545 > $$.tmp
